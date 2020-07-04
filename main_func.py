78ab86ff74ea8dc3be2680c5783d8509e8f54cc2
Damir Gilmanov <paperbam@gmail.com>
2020-07-04 19:35:19 +0300

    Added start API func and create L2 domain template func

--- main_func.py
+++ main_func.py
@@ -1,2 +1,40 @@
 from vspk import v5_0 as vspk
+from wbx_data import *
+from vsd_data import *
 import logging
+
+#Start REST API session towards to VSD
+def start_csproot_session():
+    session = vspk.NUVSDSession(
+        api_url=vsd_api_url,
+        username=vsd_api_username,
+        password=vsd_api_password,
+        enterprise=vsd_api_csp)
+    try:
+        session.start()
+    except:
+        logging.error("Unable to start the session")
+    return session.user
+
+csproot = start_csproot_session()
+
+def create_l2_domains():
+    cbis_enterprise = csproot.enterprises.get(filter='name == "{}"'.format(cbis_enterprise_name))[0]
+    new_l2_domain_template = vspk.NUL2DomainTemplate(name=l2_domain_template_name)
+    cbis_enterprise.create_child(new_l2_domain_template)
+    new_l2_domain_template.create_child(vspk.NUEgressACLTemplate(
+        name="Egress",
+        active=True,
+        default_allow_ip=True, 
+        default_allow_non_ip=True, 
+        default_install_acl_implicit_rules=True
+        ))
+    new_l2_domain_template.create_child(vspk.NUIngressACLTemplate(
+        name="Ingress",
+        active=True,
+        default_allow_ip=True, 
+        default_allow_non_ip=True,
+        allow_address_spoof=True
+        ))
+
+create_l2_domains()
---
+++ vsd_data.py
@@ -0,0 +1,11 @@
+### VSD access data
+
+vsd_api_url = 'https://localhost:8443'
+vsd_api_username = 'csproot'
+vsd_api_password = 'csproot'
+vsd_api_csp = 'csp'
+
+### VSD CBIS L2 domain variables
+cbis_enterprise_name = 'CBIS-Infrastructure-Enterprise'
+cbis_enterprise_profile_name = 'Default Profile'
+l2_domain_template_name = 'L2 Domain Template'
